64a557747d85f69ce5def9e762639e7f1836e320
Reality Inc LLC <108455405+realityincllc@users.noreply.github.com>
2022-08-13 01:15:33 -0400

    Update 8
    Bug Fixes and much more.

--- static/index.js
+++ static/index.js
@@ -7,7 +7,7 @@ form.addEventListener('submit', async event => {
         scope: __uv$config.prefix
     }).then(() => {
         let url = input.value.trim();
-        if (!isUrl(url)) url = 'https://cse.google.com/cse?cx=65ffafc4df44d1fb9#gsc.tab=0&gsc.q=' + url;
+        if (!isUrl(url)) url = 'https://ottosearchfunction.realityincorpor.repl.co/#gsc.tab=0&gsc.q=' + url;
         else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
 
 
@@ -21,4 +21,4 @@ function isUrl(val = ''){
 };
 
 
-<!--         if (!isUrl(url)) url = 'https://cse.google.com/cse?cx=65ffafc4df44d1fb9#gsc.tab=0&gsc.q=' + url; -->
+<!--         if (!isUrl(url)) url = 'https://ottosearchfunction.realityincorpor.repl.co/#gsc.tab=0&gsc.q=' + url; -->
